c6248920a3ce911e0698155ed264f9795c3301de
Dustin <hiitsdustindavis@gmail.com>
2016-01-29 08:49:49 -0800

    add initial directory, files and pass first spec

---
+++ spec/specs.js
@@ -0,0 +1,15 @@
+describe('Pizza', function() {
+
+  it("creates a new pizza with the given specifications", function() {
+    var testPizza = new Pizza("Large");
+    expect(testPizza.pizzaSize).to.equal("Large");
+    expect(testPizza.pizzaToppings).to.eql([]);
+    expect(testPizza.toppingsAdded).to.equal(0);
+  });
+
+  it("adds the addedToppings method to all pizzas", function() {
+      var testPizza = new Pizza("Feelings","Multiple Ben Afflecks");
+      expect(testPizza.addedToppings()).to.equal("Feelings, Multiple Ben Afflecks");
+  });
+
+});
