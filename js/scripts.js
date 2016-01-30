3e8fa74daa3d465518cb8ed7d2fa600dd1c1ee13
Dustin <hiitsdustindavis@gmail.com>
2016-01-29 16:03:47 -0800

    add initial business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,96 @@
-function Pizza(pizzaSize, pizzaToppings, toppingsAdded) {
+function Pizza(pizzaSize, pizzaQuantity) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = [];
-  this.toppingsAdded = 0;
+  this.pizzaQuantity = pizzaQuantity
+  this.pizzaBasePrice = 10;
 }
 
-Pizza.prototype.add = function(topping) {
-  this.pizzaToppings.push(topping);
+Pizza.prototype.sizePrice = function() {
+  if (this.pizzaSize === "Medium") {
+    return this.pizzaBasePrice;
+  } else {
+  }
+  return this.pizzaBasePrice + 5;
 };
 
-Pizza.prototype.add = function(topping) {
-  var currentPizza = this.pizzaToppings[this.toppingsAdded];
+Pizza.prototype.toppingsPrice = function() {
+  var pizzaToppings = ["Metaphysics", "FunNoodles", "Dubstep Beat Drop", "Microbes", "Feelings"];
+    for (var i = 0; 1 < pizzaToppings.length; i ++ ) {
+      if (pizzaToppings.indexOf( "Input" ) === [i] ) {
+        print( 'Yes we have ' + search + ' in the store.');
+      console.log(pizzaToppings[i]);
+//
+// Pizza.prototype.add = function(topping) {
+//   var currentPizza = this.pizzaToppings[this.toppingsAdded];
+// };
+
+
+Pizza.prototype.sizePrice = function() {
+  var price = 10;
+  var pizzaQuantity = "";
+  var userQuantity = pizzaQuantity.options[pizzaQuantity.selectedIndex].text;
+
+
+
+
+    }
+  }
 };
+
+
+
+
+// document.getElementById("myinput").onclick = function() {
+//     this.select();
+// };
+//
+// $("form#pizzaoptions").submit(function(event) {
+//     var loopEnd = $("input#count-to").val();
+//     var numbers = [counter(loopEnd)];
+//
+//     $("#output").text(numbers);
+//     $("#result").show();
+//     event.preventDefault();
+//
+//   });
+//
+//
+// function printList ( list ) {
+//   var listHTML = '<ol>';
+//   for ( var i = 0; i < list.length; i ++ ) {
+//     listHTML += '<li>' + list[i] + '</li>'
+//   }
+//   listHTML += '</ol>';
+//   print(listHTML);
+// }
+// printList(playlist);
+//
+//
+// var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
+// var search;
+//
+// function print(message) {
+//   document.write( '<p>' + message + '</p>');
+// }
+//
+// while (true) {
+// search = prompt("instrucitons");
+//   search = search.toLowerCase();
+//   if ( search === 'quit' ) {
+//     break;
+//   } else if (search === 'list') {
+//     print ( inStock.join(', '));
+//   } else {
+//     if (inStock.indexOf( search ) > -1 ) {
+//       print( 'Yes we have ' + search + ' in the store.');
+//     } else {
+//       print( search + ' is not in stock.');
+//     }
+//   }
+// }
+
+
+
 
   // var addedToppings;
   // for ( i = 0; i < addedToppings.length; i++ ) {
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,10 @@
 describe('Pizza', function() {
 
   it("creates a new pizza with the given specifications", function() {
-    var testPizza = new Pizza("Large");
+    var testPizza = new Pizza("Large", 1);
     expect(testPizza.pizzaSize).to.equal("Large");
     expect(testPizza.pizzaToppings).to.eql([]);
-    expect(testPizza.toppingsAdded).to.equal(0);
+    expect(testPizza.pizzaQuantity).to.equal(1);
   });
 
   it("adds the addedToppings method to all pizzas", function() {
