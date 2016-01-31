924ec8970ffefb2d9e4cb28d523a5a894ff5993b
Dustin <hiitsdustindavis@gmail.com>
2016-01-30 18:24:37 -0800

    debug logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,155 +1,40 @@
---- PSEUDO CODE ---
-
-One Pizza Price Calculator:
-  var sizePrice= if (size === "medium"){return pizzaBasePrice} if (size === "large"){return size + 5.}
-
-  toppings: price/topping = 1
-
-  var toppingsPrice = this.pizzaToppings.price += (var # of toppings selected)
-
-  var quantity = userinput
-
-  var onePizza = (sizePrice + toppingsPrice) * quantity
-
-
-Print to html:
-  var toppingsSelected: for loop [select list items array].join (', ')
-  
-  "Thank you for your order. Your" + pizzaSize + "pizza with" + toppingsSelected + " will cost " + onePizza + ".";
-
---- PSEUDO CODE ---
-
-function Pizza(pizzaSize, pizzaQuantity) {
+function Pizza(pizzaSize, toppings, pizzaQuantity) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = [];
   this.pizzaQuantity = pizzaQuantity
-  this.pizzaBasePrice = 10;
+  this.pizzaTotalPrice = null;
 }
 
 
-Pizza.prototype.sizePrice = function() {
+Pizza.prototype.sizePriceCalc = function() {
   if (this.pizzaSize === "Medium") {
-    return this.pizzaBasePrice;
+    return 0;
   } else {
   }
-  return this.pizzaBasePrice + 5;
-};
+    return  +5;
+}
 
-Pizza.prototype.toppingsPrice = function() {
-  var pizzaToppings = ["Metaphysics", "FunNoodles", "Dubstep Beat Drop", "Microbes", "Feelings"];
-    for (var i = 0; 1 < pizzaToppings.length; i ++ ) {
-      if (pizzaToppings.indexOf( "Input" ) === [i] ) {
-        print( 'Yes we have ' + search + ' in the store.');
-      console.log(pizzaToppings[i]);
-//
-// Pizza.prototype.add = function(topping) {
-//   var currentPizza = this.pizzaToppings[this.toppingsAdded];
-// };
+Pizza.prototype.toppingsPriceCalc = function() {
+  var totalToppings = this.pizzaToppings.length;
+  return totalToppings ;
+}
 
+Pizza.prototype.totalPriceCalc = function() {
+    return this.pizzaTotalPrice = (this.sizePriceCalc() + this.toppingsPriceCalc()) * this.pizzaQuantity;
+}
 
-Pizza.prototype.sizePrice = function() {
-  var price = 10;
-  var pizzaQuantity = "";
-  var userQuantity = pizzaQuantity.options[pizzaQuantity.selectedIndex].text;
+$(document).ready(function() {
+  $("form#pizza-options").submit(function(event) {
+    event.preventDefault();
+debugger;
+    var inputtedSize = $("select.size").val();
+    var inputtedToppings = $("select.toppings").val();
+    for (var i = 0; i < this.pizzaToppings)
+    var newPizza = new Pizza(inputtedSize,inputtedToppings);
+    //
+    // $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
-
-
-
-    }
-  }
-};
-
-
-
-
-// document.getElementById("myinput").onclick = function() {
-//     this.select();
-// };
-//
-// $("form#pizzaoptions").submit(function(event) {
-//     var loopEnd = $("input#count-to").val();
-//     var numbers = [counter(loopEnd)];
-//
-//     $("#output").text(numbers);
-//     $("#result").show();
-//     event.preventDefault();
-//
-//   });
-//
-//
-// function printList ( list ) {
-//   var listHTML = '<ol>';
-//   for ( var i = 0; i < list.length; i ++ ) {
-//     listHTML += '<li>' + list[i] + '</li>'
-//   }
-//   listHTML += '</ol>';
-//   print(listHTML);
-// }
-// printList(playlist);
-//
-//
-// var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
-// var search;
-//
-// function print(message) {
-//   document.write( '<p>' + message + '</p>');
-// }
-//
-// while (true) {
-// search = prompt("instrucitons");
-//   search = search.toLowerCase();
-//   if ( search === 'quit' ) {
-//     break;
-//   } else if (search === 'list') {
-//     print ( inStock.join(', '));
-//   } else {
-//     if (inStock.indexOf( search ) > -1 ) {
-//       print( 'Yes we have ' + search + ' in the store.');
-//     } else {
-//       print( search + ' is not in stock.');
-//     }
-//   }
-// }
-
-
-
-
-  // var addedToppings;
-  // for ( i = 0; i < addedToppings.length; i++ ) {
-  //   this.pizzaToppings[i] = [];
-  // }
-
-
-// var pizzaPrice = 0;
-
-
-
-// Playlist.prototype.add = function(song) {
-//   this.songs.push(song);
-// };
-//
-// Playlist.prototype.play = function() {
-//   var currentSong = this.songs[this.nowPlayingIndex];
-//   currentSong.play();
-// };
-//
-// Playlist.prototype.stop = function(){
-//   var currentSong = this.songs[this.nowPlayingIndex];
-//   currentSong.stop();
-// };
-//
-// Playlist.prototype.next = function() {
-//   this.stop();
-//   this.nowPlayingIndex++;
-//   if(this.nowPlayingIndex === this.songs.length) {
-//     this.nowPlayingIndex = 0;
-//   }
-//   this.play();
-// };
-//
-// Playlist.prototype.renderInElement = function(list) {
-//   list.innerHTML = "";
-//   for(var i = 0; i < this.songs.length; i++) {
-//     list.innerHTML += this.songs[i].toHTML();
-//   }
-// };
+    // $("input#new-first-name").val("");
+    // $("input#new-last-name").val("");
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,24 @@
 describe('Pizza', function() {
-
   it("creates a new pizza with the given specifications", function() {
-    var testPizza = new Pizza("Large", 1);
+    var testPizza = new Pizza("Large", 1, 2);
     expect(testPizza.pizzaSize).to.equal("Large");
     expect(testPizza.pizzaToppings).to.eql([]);
-    expect(testPizza.pizzaQuantity).to.equal(1);
+    expect(testPizza.pizzaQuantity).to.equal(2);
   });
 
-  it("adds the addedToppings method to all pizzas", function() {
-      var testPizza = new Pizza("Feelings","Multiple Ben Afflecks");
-      expect(testPizza.toppingsAdded).to.equal(0);
+  it("adds the sizePriceCalc method to all pizzas", function() {
+      var testPizza = new Pizza("Medium");
+      expect(testPizza.sizePriceCalc()).to.equal(0);
   });
 
+  it("adds the toppingsPriceCalc method to all pizzas", function() {
+      var testPizza = new Pizza([]);
+      testPizza.pizzaToppings.push("1", "2", "3")
+      expect(testPizza.toppingsPriceCalc()).to.equal(3);
+  });
+  it("adds the totalPriceCalc method to all pizzas", function() {
+      var testPizza = new Pizza("Large", [], 2);
+      testPizza.pizzaToppings.push("1", "2", "3")
+      expect(testPizza.totalPriceCalc()).to.equal(16);
+  });
 });
