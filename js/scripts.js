277fde15e3bcb59e9ccff368b1403a82b567b0c9
Dustin <hiitsdustindavis@gmail.com>
2016-01-31 10:36:10 -0800

    adjust totalPriceCalc() logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,17 @@ function Pizza(pizzaSize, toppings, pizzaQuantity) {
 
 
 Pizza.prototype.sizePriceCalc = function() {
+  var sizeVal = 0
   if (this.pizzaSize === "Medium") {
     return 0;
   } else {
   }
-    return  +5;
+    return sizeVal + 5;
 }
 
 Pizza.prototype.toppingsPriceCalc = function() {
-  var totalToppings = this.pizzaToppings.length;
-  return totalToppings ;
+  var totalToppings = this.pizzaToppings;
+  return totalToppings;
 }
 
 Pizza.prototype.totalPriceCalc = function() {
@@ -29,10 +30,12 @@ $(document).ready(function() {
 debugger;
     var inputtedSize = $("select.size").val();
     var inputtedToppings = $("select.toppings").val();
+    var inputtedToppingsVal = inputtedToppings.length;
     var toppingString = inputtedToppings.join(", ")
-    var inputtedQuantity = $("input#quantity").val();
-    var totalPrice = $(Pizza.prototype.totalPriceCalc()).val();
+    var inputtedQuantity = parseInt($("input#quantity").val());
+
     var newPizza = new Pizza(inputtedSize, inputtedToppings, inputtedQuantity);
+    var totalPrice = (inputtedSizeVal + inputtedToppingsVal) * inputtedQuantity;
 
     $("div.output").append('<p>Thank you for ordering ' + inputtedQuantity + " " + toppingString + ' pizza(s).</p></br><p>Your total is ' + totalPrice);
 
